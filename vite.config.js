1679ca067ac02c5ed6e12ac384a3a9b51c1f2bc6
myphic <suvorov.alexander15@gmail.com>
2024-01-02 15:12:06 +0300

    Добавлен пакет laravel breeze

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,17 @@ export default defineConfig({
         },
     },
     plugins: [
-        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
